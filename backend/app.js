f44aadb59171af813097d71e2841dbc568c23095
Aditya mishra <adityamishra6674.com>
2026-01-29 17:07:22 +0530

    add Cloudinary and MongoDB configuration files

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 require('dotenv').config()
-const db = require('./config/db');
+const db = require('../.vscode/config/db');
 db();
 const authRoutes = require('./routes/Auth.routes');
 const adminRoutes = require('./routes/admin.routes');
